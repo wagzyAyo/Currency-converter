7612cf36c35030730dfe66426c78eb0a781a2d65
Ayo <talktojmcvibes@gmail.com>
2024-01-27 16:29:57 +0100

    fix repetition

--- main.py
+++ main.py
@@ -39,22 +39,16 @@ response = requests.get(endpoint).json()
 def convert(from_c, to_c, amount):
    '''Handle currency conversion rates'''
    try:
-    
-    if from_c in response['rates'] and to_c in response['rates']:
-        #rate = response['rates'][to_c]
-        #converted_amount = rate * int(amount)
-        #converted_amount = round(converted_amount, 2)
-        #return converted_amount
-        if from_c in response['rates'] and to_c in response['rates']:
-            rate_from = Decimal(response['rates'][from_c])
-            rate_to = Decimal(response['rates'][to_c])
-            
-            convert_amount = (Decimal(amount) / rate_from) * rate_to
-            convert_amount = round(convert_amount, 2)
-            
-            return convert_amount
-    else:
-        return('Invalid conversion')
+      if from_c in response['rates'] and to_c in response['rates']:
+         rate_from = Decimal(response['rates'][from_c])
+         rate_to = Decimal(response['rates'][to_c])
+         
+         convert_amount = (Decimal(amount) / rate_from) * rate_to
+         convert_amount = round(convert_amount, 2)
+         
+         return convert_amount
+      else:
+         return('Invalid conversion')
    except Exception as e:
       return f'Error: {str(e)}'
    
