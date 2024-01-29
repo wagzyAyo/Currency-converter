54d1f26576aa46abe71c5b135b296de5c763c7ae
Ayo <talktojmcvibes@gmail.com>
2024-01-29 10:56:16 +0100

    convert api

--- api/convert.py
+++ api/convert.py
@@ -3,13 +3,13 @@ from utils import convert, unit_per
 
 Bp = Blueprint('Bp', __name__)
 
-@Bp.route('/<convert_from>/<convert_to>/<int: amount>')
-def convert(convert_from, convert_to, amount):
-    convert_from = int(convert_from)
-    convert_to = int(convert_to)
+@Bp.route('/<convert_from>/<convert_to>/<int:amount>')
+def convert_api(convert_from, convert_to, amount):
+    convert_from = convert_from
+    convert_to = convert_to
     amount = int(amount)
     convert_result = convert(convert_from, convert_to, amount)
-    unit_result = unit_per(convert_from, convert_to, amount)
+    unit_result = unit_per(convert_from, convert_to, amount, convert_result)
     return ({'convert_from': convert_from,
              'convert_to': convert_to,
              'amount_to_convert': amount,
