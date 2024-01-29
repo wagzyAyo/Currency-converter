2854db9107504b89f1b838b809a96e77703e65b0
Ayo <talktojmcvibes@gmail.com>
2024-01-29 21:19:38 +0100

    logo

--- models/base_model.py
+++ models/base_model.py
@@ -6,6 +6,7 @@ from sqlalchemy.orm import DeclarativeBase
 class Base(DeclarativeBase):
 
     def __init__(self):
+        """initialize base class"""
         date = datetime.now()
         id = uuid4()
 
