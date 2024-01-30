041c9cd67db5d4a1c1b53a04bf121a9528994133
Ayo <talktojmcvibes@gmail.com>
2024-01-30 09:48:53 +0100

    base_model

--- models/base_model.py
+++ models/base_model.py
@@ -3,25 +3,34 @@ from datetime import datetime
 from uuid import uuid4
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, String, DateTime
+from sqlalchemy.orm import Session
 from dotenv import load_dotenv
 import os
 import requests
+import json
 
-base = declarative_base
+base = declarative_base()
 load_dotenv()
 engine = create_engine('sqlite+pysqlite:///:memory:', echo=True)
 
-class Base(base):
+class CurrencyData(base):
     __tablename__ = 'currency_data'
+    id = Column(String, primary_key=True, default=str(uuid4()))
+    date = Column(DateTime, default=datetime.now())
+    endpoint = os.getenv('endpoint')
+    data = Column(String, default=json.dumps(requests.get(endpoint).json()))
 
 
+# Create tables in the database
+base.metadata.create_all(engine)
 
-    def __init__(self):
-        """initialize base class"""
-        self.date = datetime.now()
-        self.id = uuid4()
-        endpoint = os.getenv('endpoint')
-        self.data = requests.get(endpoint).json()
+# Create a session to interact with the database
+session = Session(engine)
 
-base = Base()
+# Create an instance of CurrencyData
+currency_instance = CurrencyData()
+
+# Add the instance to the session and commit changes
+session.add(currency_instance)
+session.commit()
