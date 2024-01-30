2baf826347448d19c9661c6951a702190fb0f935
Ayo <talktojmcvibes@gmail.com>
2024-01-30 09:36:56 +0100

    base

--- models/base_model.py
+++ models/base_model.py
@@ -1,14 +1,27 @@
 """Base clase declaration"""
 from datetime import datetime
 from uuid import uuid4
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String
+from dotenv import load_dotenv
+import os
+import requests
 
-class Base(DeclarativeBase):
+base = declarative_base
+load_dotenv()
+engine = create_engine('sqlite+pysqlite:///:memory:', echo=True)
+
+class Base(base):
+    __tablename__ = 'currency_data'
+
+
 
     def __init__(self):
         """initialize base class"""
-        date = datetime.now()
-        id = uuid4()
-
+        self.date = datetime.now()
+        self.id = uuid4()
+        endpoint = os.getenv('endpoint')
+        self.data = requests.get(endpoint).json()
 
 base = Base()
