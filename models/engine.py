2d2dd8e023c01ffa70ceec6667511f49ccf8df7d
Ayo <talktojmcvibes@gmail.com>
2024-02-01 13:39:23 +0100

    engine refix

--- models/engine.py
+++ models/engine.py
@@ -1,6 +1,6 @@
-from pymongo import MongoClient
-import requests
 from dotenv import load_dotenv
+import requests
+from pymongo import MongoClient
 import os
 
 client = MongoClient("mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000")
@@ -13,18 +13,23 @@ def get_database():
     load_dotenv()
     try:
         endpoint = os.getenv('endpoint')
+
+        client = MongoClient("mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000")
+
         result = requests.get(endpoint)
         if result.status_code != 200:
-            endpoint2 = os.getenv('endpoint2')
-            result = requests.get(endpoint2)
+            endpoint = os.getenv('endpoint2')
+            result = requests.get(endpoint)
         result = result.json()
         currency_data.insert_one(result)
     except Exception as e:
-        return f"An error ocured : {e}"
+        return
     finally:
+        #for data in currency_data.find():
+        #    print(data)
         #for data in currency_data.find():
         #    print(data)
         client.close()
 
 
-client.close()
+get_database()
