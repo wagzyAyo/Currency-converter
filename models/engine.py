1b7d83ef7f84c6b433db8c8570237b90eef4dcdb
Ayo <talktojmcvibes@gmail.com>
2024-02-01 14:29:29 +0100

    schedule task

--- models/engine.py
+++ models/engine.py
@@ -2,6 +2,8 @@ from dotenv import load_dotenv
 import requests
 from pymongo import MongoClient
 import os
+import schedule
+import time
 
 client = MongoClient("mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000")
 
@@ -13,8 +15,6 @@ def get_database():
     load_dotenv()
     try:
         endpoint = os.getenv('endpoint')
-
-        client = MongoClient("mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000")
 
         result = requests.get(endpoint)
         if result.status_code != 200:
@@ -31,5 +31,13 @@ def get_database():
         #    print(data)
         client.close()
 
+schedule.every().day.at("04:00").do(get_database)
 
-get_database()
+schedule.every().day.at("12:00").do(get_database)
+
+schedule.every().day.at("21:00").do(get_database)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
+    
