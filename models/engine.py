b56d29b2c5b33ef442d79239bfe7ec7b44c7e985
Ayo <talktojmcvibes@gmail.com>
2024-02-01 13:05:16 +0100

    pymongo

--- models/engine.py
+++ models/engine.py
@@ -13,17 +13,18 @@ def get_database():
     load_dotenv()
     try:
         endpoint = os.getenv('endpoint')
-
-        result = requests.get(endpoint).json()
+        result = requests.get(endpoint)
+        if result.status_code != 200:
+            endpoint = os.getenv('endpoint2')
+            result = requests.get(endpoint)
+        result = result.json()
         currency_data.insert_one(result)
     except Exception as e:
-        try:
-            endpoint2 = os.getenv('endpoint2')
-            result = requests.get(endpoint2).json()
-            currency_data.insert_one(result)
-        except Exception as e:
-            return
+        return f"An error ocured : {e}"
     finally:
         #for data in currency_data.find():
         #    print(data)
         client.close()
+
+
+client.close()
