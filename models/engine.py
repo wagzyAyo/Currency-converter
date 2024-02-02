4be37ebd104caaee20968bd6ab393565e0f920a0
Ayo <talktojmcvibes@gmail.com>
2024-02-02 15:06:32 +0100

    engine

--- models/engine.py
+++ models/engine.py
@@ -49,15 +49,14 @@ def get_database():
         client.close()
 
 wat_4 = convert_to_wat('04:00', 'Africa/Lagos')
-schedule.every().day.at(str(wat_4)).do(get_database)
+schedule.every().day.at(str(wat_4.strftime('%H:%M'))).do(get_database)
 
 wat_12 = convert_to_wat('12:00', 'Africa/Lagos')
-schedule.every().day.at(str(wat_12)).do(get_database)
+schedule.every().day.at(str(wat_12.strftime('%H:%M'))).do(get_database)
 
 wat_21 = convert_to_wat('21:00', 'Africa/Lagos')
-schedule.every().day.at(str(wat_21)).do(get_database)
+schedule.every().day.at(str(wat_21.strftime('%H:%M'))).do(get_database)
 
 while True:
     schedule.run_pending()
     time.sleep(1)
-    
