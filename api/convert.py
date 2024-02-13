ad905b3e3d5066bfdbd47a7bb5fdfc4e4c0eff35
Ayo <talktojmcvibes@gmail.com>
2024-02-13 10:13:47 +0100

    uppercase key

--- api/convert.py
+++ api/convert.py
@@ -7,8 +7,8 @@ Bp = Blueprint('Bp', __name__)
 
 @Bp.route('/<convert_from>/<convert_to>/<int:amount>')
 def convert_api(convert_from, convert_to, amount):
-    convert_from = convert_from
-    convert_to = convert_to
+    convert_from = str(convert_from).upper()
+    convert_to = str(convert_to).upper()
     amount = int(amount)
     convert_result = convert(convert_from, convert_to, amount)
     unit_result = unit_per(convert_from, convert_to, amount, convert_result)
